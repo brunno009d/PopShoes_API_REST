d0b8712b88dbf35b41442af13b2132cabef327ad
MauriLS <mauriciolillo122@gmail.com>
2025-11-23 18:59:45 -0300

    fix usuarioControler

--- src/main/java/com/example/backend/controller/UsuarioController.java
+++ src/main/java/com/example/backend/controller/UsuarioController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.backend.model.Usuario;
 import com.example.backend.service.UsuarioService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/usuarios")
