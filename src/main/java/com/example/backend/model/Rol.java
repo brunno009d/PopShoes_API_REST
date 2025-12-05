ef59fd95ee3151cc87ca80854fc29f2c809c73e2
MauriLS <mauriciolillo122@gmail.com>
2025-12-04 22:52:25 -0300

    FIX data

--- src/main/java/com/example/backend/model/Rol.java
+++ src/main/java/com/example/backend/model/Rol.java
@@ -7,10 +7,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Data
+@Getter   
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Rol {
